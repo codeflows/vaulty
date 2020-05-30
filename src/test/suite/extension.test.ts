1bf7252a398d668002a2027e0dc63bc053ae5a89
Jari Aarniala <jari@aarniala.fi>
2020-05-30 21:19:54 +0300

    Improve basic test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,8 +1,12 @@
 import * as vscode from 'vscode'
+import * as assert from 'assert'
 
 suite('Vaulty', () => {
   test('can be activated', async () => {
     const vaulty = vscode.extensions.getExtension('codeflows.vaulty')
-    await vaulty?.activate()
+    assert.ok(vaulty)
+    await vaulty.activate()
+    const commands = await vscode.commands.getCommands()
+    assert.ok(commands.includes('vaulty.decrypt'))
   })
 })
