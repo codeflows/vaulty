c5bb149564d075fbb33fd2be83808cec07a5fb03
Jari Aarniala <jari@aarniala.fi>
2020-05-31 23:10:07 +0300

    Make tests more robust

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -2,7 +2,36 @@ import * as vscode from 'vscode'
 import * as assert from 'assert'
 import { teardown } from 'mocha'
 
+const delay = (delayMs: number) => new Promise((resolve) => setTimeout(resolve, delayMs))
+
+const waitTickMs = 50
+
+const waitFor = async (condition: () => boolean, timeLeftMs: number = 5000): Promise<void> => {
+  if (condition()) {
+    return
+  } else if (timeLeftMs <= 0) {
+    throw new Error('Timed out waiting for condition')
+  } else {
+    await delay(waitTickMs)
+    return waitFor(condition, timeLeftMs - waitTickMs)
+  }
+}
+
+const decryptedFileIsOpen = () => vscode.window.activeTextEditor?.document.uri.scheme === 'vaulty'
+
+const getWorkspaceRootPath = () => {
+  const folders = vscode.workspace.workspaceFolders
+  assert.ok(folders)
+  assert.equal(folders.length, 1)
+
+  return folders[0].uri
+}
+
 suite('Vaulty', () => {
+  teardown(async () => {
+    await vscode.commands.executeCommand('workbench.action.closeAllEditors')
+  })
+
   test('can be activated', async () => {
     const vaulty = vscode.extensions.getExtension('codeflows.vaulty')
     assert.ok(vaulty)
@@ -11,25 +40,16 @@ suite('Vaulty', () => {
     assert.ok(commands.includes('vaulty.decrypt'))
   })
 
-  teardown(async () => {
-    await vscode.commands.executeCommand('workbench.action.closeAllEditors')
-  })
-
   test('can run decrypt on a sample file', async () => {
-    const folders = vscode.workspace.workspaceFolders
-    assert.ok(folders)
-    assert.equal(folders.length, 1)
+    const secretsUri = vscode.Uri.joinPath(getWorkspaceRootPath(), 'configuration_in_same_directory', 'secrets.yml')
 
-    const root = folders[0].uri
-    const secrets = vscode.Uri.joinPath(root, 'configuration_in_same_directory', 'secrets.yml')
-
-    const textDocument = await vscode.workspace.openTextDocument(secrets)
+    const textDocument = await vscode.workspace.openTextDocument(secretsUri)
     await vscode.window.showTextDocument(textDocument)
     await vscode.commands.executeCommand('vaulty.decrypt')
 
-    await new Promise((resolve) => setTimeout(resolve, 1000))
+    await waitFor(decryptedFileIsOpen)
     const editor = vscode.window.activeTextEditor
     assert.ok(editor)
     assert.ok(editor.document.getText().includes('test: true'))
-  }).timeout(10000)
+  })
 })
