5f8625df602f7d249f13224e66b64871b8138f03
Jari Aarniala <jari@aarniala.fi>
2020-05-31 16:13:22 +0300

    Open `test-vaults` directory in extension tests

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -12,8 +12,14 @@ async function main() {
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './suite/index')
 
+    const testWorkspace = path.resolve(__dirname, extensionDevelopmentPath + '/test-vaults/')
+
     // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath })
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: [testWorkspace, '--disable-extensions']
+    })
   } catch (err) {
     console.error('Failed to run tests')
     process.exit(1)
